c878e6e912f1c6882ba97d8e26e83333c809f466
mht20 <mht252028@gmail.com>
2023-03-07 13:44:36 +0000

    Insert into table

---
+++ problem1.sql
@@ -0,0 +1,5 @@
+
+
+insert into song (title , artist) values('Teacher ' , 'Danser');
+insert into song (title , artist) values('Auther ' , 'Painter');
+insert into song (title , artist) values('Gamer ' , 'Lad Zeppelin');
